dcd39a0eda20dd6c98d6c2c45e331a69f117cb30
Bruno Henrique <bruninho.carvalho159@gmail.com>
2018-11-07 11:18:56 -0200

    First commit

---
+++ Tela1/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Tela1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lbxContatos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(16, 9);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(76, 25);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "label1";
+            // 
+            // lbxContatos
+            // 
+            this.lbxContatos.FormattingEnabled = true;
+            this.lbxContatos.ItemHeight = 25;
+            this.lbxContatos.Location = new System.Drawing.Point(12, 74);
+            this.lbxContatos.Name = "lbxContatos";
+            this.lbxContatos.Size = new System.Drawing.Size(432, 154);
+            this.lbxContatos.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 240);
+            this.Controls.Add(this.lbxContatos);
+            this.Controls.Add(this.lblTitulo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.ListBox lbxContatos;
+    }
+}
+
---
+++ Tela1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Tela1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblTitulo.Text = CarregarTitulo();
+            CriarContato();
+            CarregarContatos();
+        }
+
+        private string CarregarTitulo()
+        {
+            XmlDocument documentoXML = new XmlDocument();
+
+            documentoXML.Load(@"C:\Users\Patty\Google Drive\Treinamento C#\XML\Tela1\Agenda.xml");
+
+            XmlNode noTitulo = documentoXML.SelectSingleNode("/agenda/titulo");
+
+            return noTitulo.InnerText;
+        }
+
+        private void CarregarContatos()
+        {
+            XmlDocument documentoXML = new XmlDocument();
+
+            documentoXML.Load(@"C:\Users\Patty\Google Drive\Treinamento C#\XML\Tela1\Agenda.xml");
+
+            XmlNodeList contatos = documentoXML.SelectNodes("/agenda/contatos/contato");
+
+            foreach (XmlNode node in contatos)
+            {
+                string representacao = "";
+                string id = node.Attributes["id"].Value;
+                string nome = node.Attributes["nome"].Value;
+                string idade = node.Attributes["idade"].Value;
+                representacao = id + " " + nome + " " + idade;
+                lbxContatos.Items.Add(representacao);
+            }
+
+        }
+        private void CriarContato()
+        {
+            XmlDocument documentoXML = new XmlDocument();
+            documentoXML.Load(@"C:\Users\Patty\Google Drive\Treinamento C#\XML\Tela1\Agenda.xml");
+            XmlAttribute atributeId = documentoXML.CreateAttribute("id");
+            XmlAttribute atributeNome = documentoXML.CreateAttribute("nome");
+            XmlAttribute atributeIdade = documentoXML.CreateAttribute("idade");
+
+            atributeId.Value = "5";
+            atributeNome.Value = "Jão";
+            atributeIdade.Value = "35";
+
+            XmlNode novoContato = documentoXML.CreateElement("contato");
+
+            novoContato.Attributes.Append(atributeId);
+            novoContato.Attributes.Append(atributeNome);
+            novoContato.Attributes.Append(atributeIdade);
+
+            XmlNode contatos = documentoXML.SelectSingleNode("/agenda/contatos");
+            contatos.AppendChild(novoContato);
+
+            documentoXML.Save(@"C:\Users\Patty\Google Drive\Treinamento C#\XML\Tela1\Agenda.xml");
+        }
+    }
+}
